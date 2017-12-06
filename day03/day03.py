426138f6af015ded83e51492468ac51f017747b7
Brad Myers <myersbrad77@gmail.com>
2017-12-05 19:40:18 -0500

    day 5

--- day03/day03.py
+++ day03/day03.py
@@ -1,32 +1,3 @@
-# input = 347991
-
-# x = 0
-# y = 0
-# d = 1
-# m = 1
-
-# count = 1
-
-# array = dict()
-
-# while count < input:
-#   while 2 * x * d < m and count <= input:
-#     if count == input:
-#       print(abs(x) + (abs(y)))
-#       break
-#     array[(x, y)] = count
-#     x = x + d
-#     count += 1
-#   while 2 * y * d < m and count <= input:
-#     if count == input:
-#       print(abs(x) + (abs(y)))
-#       break
-#     array[(x, y)] = count
-#     y = y + d
-#     count += 1
-#   d = -1 * d
-#   m = m + 1
-
 import operator
 
 input = 347991
--- day04/day04.exs
+++ day04/day04.exs
@@ -1,36 +1,35 @@
-# input = File.read('input.txt').chomp
-
-# puts input.each_line.inject(0) { |sum, line| line.split.uniq! ? sum : sum + 1 }
-# puts input.each_line.inject(0) { |sum, line| line.split.map!(&:chars).map!(&:sort).uniq! ? sum : sum + 1 }
-
-# File.read!("input.txt")
-
-
-def create_deck do
-  values = ["Ace", "Two", "Three", "Four", "Five"]
-  suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-
-  for suit <- suits, value <- values do
-    "#{value} of #{suit}"
+defmodule Day04 do
+  def no_dups?(sentence) do
+    words = String.split(sentence)
+    words == Enum.uniq(words)
   end
 end
 
-def load(sentence, acc) do
-  String.split(sentence)
-  |> Enum.map(String.graphemes |> Enum.sort)
-
-
-  |> Enum.sort
-
-  case File.read(filename) do
-    {:ok, binary} -> :erlang.binary_to_term binary
-    {:error, _reason} -> "That file does not exist"
-  end
-end
-
-"testing"
-|> String.graphemes
-|> Enum.sort
+File.read!("input.txt")
+|> String.split("\n", trim: true)
+|> Enum.filter(&Day04.no_dups?/1)
+|> length
 |> IO.puts
 
-Enum.reduce([1, 2, 3], 0, fn(x, acc) -> x + acc end)
+File.read!("input.txt")
+|> String.split("\n", trim: true)
+|> Enum.filter(&Day04.no_dups?/1)
+|> length
+|> IO.puts
+
+# File.read!("input.txt")
+# |> String.trim
+# |> String.split("\n")
+# |> Enum.reduce(0, &Day04.check_dups/2)
+# |> IO.puts
+
+# "here is a test"
+# |> String.split
+# |> Enum.sort
+# |> IO.inspect
+
+# "testing"
+# |> String.graphemes
+# |> Enum.sort
+# |> IO.inspect
+
