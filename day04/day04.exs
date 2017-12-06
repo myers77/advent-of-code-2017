5ae452b024d2787423bf3547b05fbe7ca7a3cd01
Brad Myers <myersbrad77@gmail.com>
2017-12-06 12:07:44 -0500

    day 6 update

--- day04/day04.exs
+++ day04/day04.exs
@@ -31,20 +31,3 @@ File.read!("input.txt")
 |> Enum.filter(&Day04.no_dups?/1)
 |> length
 |> IO.puts
-
-# File.read!("input.txt")
-# |> String.trim
-# |> String.split("\n")
-# |> Enum.reduce(0, &Day04.check_dups/2)
-# |> IO.puts
-
-# "here is a test"
-# |> String.split
-# |> Enum.sort
-# |> IO.inspect
-
-# "testing"
-# |> String.graphemes
-# |> Enum.sort
-# |> IO.inspect
-
--- day06/day06.rb
+++ day06/day06.rb
@@ -1,39 +1,27 @@
-input = File.read('input.txt').chomp.split.each.map(&:to_i)
-
-# puts input.each_line.inject(0) { |sum, line| line.split.uniq! ? sum : sum + 1 }
-# puts input.each_line.inject(0) { |sum, line| line.split.map!(&:chars).map!(&:sort).uniq! ? sum : sum + 1 }
-
-# puts input.each_line.select{ |line| !line.split.uniq! }.length
-# puts input.each_line.select{ |line| !line.split.map!(&:chars).map!(&:sort).uniq! }.length
-steps = 0
-states = []
-states << input.dup.hash
-
-while states.uniq == states
-  new_input = input
+def redistribute(input)
   max = input.max
-  index = input.find_index(input.max) + 1
-  input[input.find_index(input.max)] = 0
-  remaining = max
+  index = input.index(input.max) + 1
+  input[input.index(input.max)] = 0
 
-  while remaining > 0
-    if index == input.size
-      index = 0
-    end
-    input[index] += 1
-    remaining -= 1
+  (1..max).to_a.each do
+    input[index % input.size] += 1
     index += 1
   end
 
-  states << input.dup.hash
-  steps += 1
+  return input
 end
-puts steps
-puts states.index(states.last)
-p states.each_index.select{|i| states[i] == states.last} # =>[0, 2, 6]
-# input.each_with_index do |val, i|
-#   next if i == input.find_index(input.max) 
 
-# end
+require 'benchmark'
 
-# while 
+puts Benchmark.measure {
+input = File.read('input.txt').chomp.split.each.map(&:to_i)
+states = Hash.new()
+
+while !states[input.hash]
+  states[input.hash] = states.size
+  input = redistribute(input.dup)
+end
+
+puts states.size
+puts states.size - states[input.hash]
+}
