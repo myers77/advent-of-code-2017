5853e10dc5cb3f9dab360fe75aa7ba326d9f0013
Brad Myers <myersbrad77@gmail.com>
2017-12-08 00:50:50 -0500

    day 8

--- day08/day08.rb
+++ day08/day08.rb
@@ -2,32 +2,16 @@ input = File.read('input.txt').chomp
 
 vars = Hash.new
 vars.default = 0
-
 highest = 0
 
 input.each_line do |line|
   matches = /^(\w+) (\w+) (-?\d+) if (\w+) (.+) (-?\d+)/.match(line)
-
-  if !vars.empty? && vars.values.max > highest
-    highest = vars.values.max
-  end
+  highest = [highest, vars.values.max].max if !vars.empty?
 
   if vars[matches[4]].public_send(matches[5], matches[6].to_i)
-    puts matches[0]
-    if matches[2] == 'inc'
-      vars[matches[1]] += matches[3].to_i
-    else
-      vars[matches[1]] -= matches[3].to_i
-    end
+    vars[matches[1]] += matches[2] == 'inc' ? matches[3].to_i : -1 * matches[3].to_i
   end
 end
 
-def largest_hash_key(hash)
-  hash.max_by{|k,v| v}
-end
-
-puts vars
-puts largest_hash_key(vars)
+puts vars.values.max
 puts highest
-
-# 6839
