a9daa2cd5600af8b0872a13e7c01f83d67627244
Brad Myers <myersbrad77@gmail.com>
2017-12-11 10:44:34 -0500

    added testing

--- day11/day11.rb
+++ day11/day11.rb
@@ -1,14 +1,49 @@
-input = File.read('input.txt').chomp.split(",")
+require "test/unit"
 
-max = 0
-moves = Hash.new
-moves.default = 0
+def run(input)
+  max = 0
+  coords = { x: 0, y: 0, z: 0 }
 
-input.each do |i|
-  moves[i] += 1
+  input.each do |i|
+    coords = step(coords, i)
+    max = [distance(coords), max].max
+  end
 
-  a = moves["nw"] - moves["se"]
-  max = [(moves["s"] - moves["n"] - a).abs + (moves["ne"] - moves["sw"] - a).abs, max].max
+  return [distance(coords), max]
 end
 
-puts max
+def step(coords, dir)
+  case dir
+    when 'n'
+      coords[:y] += 1
+      coords[:z] -= 1
+    when 'ne'
+      coords[:x] += 1
+      coords[:z] -= 1
+    when 'nw'
+      coords[:y] += 1
+      coords[:x] -= 1
+    when 's'
+      coords[:y] -= 1
+      coords[:z] += 1
+    when 'se'
+      coords[:y] -= 1
+      coords[:x] += 1
+    when 'sw'
+      coords[:x] -= 1
+      coords[:z] += 1
+  end
+
+  coords
+end
+
+def distance(coords)
+  coords.values.map(&:abs).reduce(:+) / 2
+end
+
+puts 3 == run(['ne', 'ne', 'ne'])[0]
+puts 0 == run(['ne', 'ne', 'sw', 'sw'])[0]
+puts 2 == run(['ne', 'ne', 's', 's'])[0]
+puts 3 == run(['se', 'sw', 'se', 'sw', 'sw'])[0]
+
+puts run(File.read('input.txt').chomp.split(","))
